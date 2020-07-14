ca51eb565689b59454417fc8e5b15db902c4bb66
Carlos Oriol <oriol.carlos3@gmail.com>
2020-07-14 01:28:00 -0400

    Loading animation

--- src/Components/Characters/CharacterGrid.js
+++ src/Components/Characters/CharacterGrid.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './character-grid.css'
 
 import Character from './Character/Character'
+import Loading from '../Loading/Loading'
+
 const CharacterGrid = (props) => {
 
     return (
-        <div>
-            {props.isLoading ? <p>Loading...</p>: <div className='character-grid'>{props.characters.map((character)=><Character key={character.char_id} name={character.name} nickname={character.nickname} portrayed={character.portrayed} status={character.status} img={character.img}/> )} </div>}
-        </div>
+        <>
+            {props.isLoading ? <Loading />  : <div className='character-grid'>{props.characters.map((character)=><Character key={character.char_id} name={character.name} nickname={character.nickname} portrayed={character.portrayed} status={character.status} img={character.img}/> )} </div>}
+        </>
     )
 }
 
---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './loading.css'
+
+const Loading = () => <div className='loading-container'><div className='loading'></div></div>
+
+export default Loading
