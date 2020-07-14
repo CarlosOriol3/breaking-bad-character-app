2a62aa361feb7199c1098288db3133640c07a99f
Carlos Oriol <oriol.carlos3@gmail.com>
2020-07-13 22:08:50 -0400

    Added css to character cards

--- src/Components/Characters/Character/Character.js
+++ src/Components/Characters/Character/Character.js
@@ -7,9 +7,20 @@ const Character = (props) => {
         <div className='card-container'>
             <div className='card'>
                 <div className='card-front' style={{ backgroundImage: `url(${props.img})`, }}></div>
-            <div className='card-back'><h1>{props.name}</h1> </div>
-        </div>
-    </div >
+                <div className='card-back'>
+                    <h1>{props.name}</h1>
+                    <hr />
+                    <div className='character-info'>
+                        <p>NickName:</p>
+                        <h3>{props.nickname}</h3>
+                        <p>Portrayed by:</p>
+                        <h3>{props.portrayed}</h3>
+                        <p>Status:</p>
+                        <h3>{props.status}</h3>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
--- src/Components/Characters/CharacterGrid.js
+++ src/Components/Characters/CharacterGrid.js
@@ -6,7 +6,7 @@ const CharacterGrid = (props) => {
 
     return (
         <div>
-            {props.isLoading ? <p>Loading...</p>: <div className='character-grid'>{props.characters.map((character)=><Character key={character.char_id} name={character.name} img={character.img}/> )} </div>}
+            {props.isLoading ? <p>Loading...</p>: <div className='character-grid'>{props.characters.map((character)=><Character key={character.char_id} name={character.name} nickname={character.nickname} portrayed={character.portrayed} status={character.status} img={character.img}/> )} </div>}
         </div>
     )
 }
